623d9cf2d8b5867881441825e741634471c0b1fe
Steve-Rye <3205355172@qq.com>
2025-02-13 00:23:51 +0800

    修改重命名逻辑以包含设备信息，若文件完全相同则自动编号

--- rename.py
+++ rename.py
@@ -7,23 +7,44 @@ def parse_date(date_str):
     try:
         if '+' in date_str:
             date_str = date_str.split('+')[0]
-        return datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S').strftime('%y%m%d%H%M%S')
+        return datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
     except ValueError:
         return None
 
-def get_exif_date(et, image_path):
-    """获取图片的拍摄时间"""
+def get_device_name(metadata):
+    """获取设备名称"""
+    model_keys = ['EXIF:Model', 'IFD0:Model']
+    make_keys = ['EXIF:Make', 'IFD0:Make']
+    
+    # 尝试获取设备型号
+    for key in model_keys:
+        if key in metadata and metadata[key]:
+            return metadata[key].replace(' ', '_')
+    
+    # 如果没有型号，尝试获取制造商
+    for key in make_keys:
+        if key in metadata and metadata[key]:
+            return metadata[key].replace(' ', '_')
+    
+    return 'Unknown'
+
+def get_exif_info(et, image_path):
+    """获取图片的拍摄时间和设备信息"""
     try:
         metadata = et.get_metadata(image_path)[0]
         date_keys = ['EXIF:DateTimeOriginal', 'EXIF:CreateDate', 'XMP:CreateDate', 'EXIF:ModifyDate']
         
+        date_time = None
         for key in date_keys:
             if key in metadata and (date := parse_date(metadata[key])):
-                return date
-        return None
+                date_time = date
+                break
+                
+        device = get_device_name(metadata)
+        return date_time, device
     except Exception as e:
         print(f"处理 {image_path} 时出错: {str(e)}")
-        return None
+        return None, None
 
 def rename_images(input_folder):
     """重命名文件夹中的所有图片"""
@@ -35,15 +56,16 @@ def rename_images(input_folder):
                 continue
                 
             file_path = os.path.join(input_folder, filename)
-            if date_time := get_exif_date(et, file_path):
+            date_time, device = get_exif_info(et, file_path)
+            
+            if date_time:
                 file_ext = os.path.splitext(filename)[1]
-                new_name = f"{date_time}{file_ext}"
-                new_path = os.path.join(input_folder, new_name)
-                
                 counter = 1
-                while os.path.exists(new_path):
-                    new_name = f"{date_time}_{counter}{file_ext}"
+                while True:
+                    new_name = f"{device}_{date_time}_{counter}{file_ext}"
                     new_path = os.path.join(input_folder, new_name)
+                    if not os.path.exists(new_path):
+                        break
                     counter += 1
                 
                 try:
